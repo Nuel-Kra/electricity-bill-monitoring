165c917cf013f04dabda06dcbdfb248d4c614202
Nuel-Kra <k.emmlkrah@gmail.com>
2026-02-28 14:09:45 +0000

    Implement appliance registration feature

--- main.cpp
+++ main.cpp
@@ -16,14 +16,35 @@ int main() {
 
     do {
         cout << "\n--- Electrical Load Monitoring ---\n";
-        cout << "1. Exit\n";
+        cout << "1. Add Appliance\n";
+        cout << "2. Exit\n";
         cout << "Choice: ";
         cin >> choice;
 
-        if (choice != 1)
-            cout << "Invalid choice.\n";
+        if (choice == 1) {
+            if (count >= MAX) {
+                cout << "Limit reached.\n";
+                continue;
+            }
 
-    } while (choice != 1);
+            cout << "Name: ";
+            cin.ignore();
+            getline(cin, appliances[count].name);
+
+            cout << "Watts: ";
+            cin >> appliances[count].watts;
+
+            cout << "Hours per day: ";
+            cin >> appliances[count].hours;
+
+            count++;
+            cout << "Appliance added.\n";
+        }
+        else if (choice != 2) {
+            cout << "Invalid choice.\n";
+        }
+
+    } while (choice != 2);
 
     cout << "Goodbye.\n";
     return 0;
